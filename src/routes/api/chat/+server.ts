77b57e3934638a1288c599a7fc84b1cbd5ec8054
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2023-12-13 17:52:19 +0100

    getting youtube links to movie recommendations

--- src/routes/api/chat/+server.ts
+++ src/routes/api/chat/+server.ts
@@ -1,9 +1,49 @@
+import { YOUTUBE_API_KEY } from '$env/static/private';
 import { OpenAIStream, StreamingTextResponse } from 'ai';
 import { openai } from '$lib/server/openai';
 
 import type { RequestHandler } from './$types';
 import { supabase } from '$lib/supabase';
 import { error } from '@sveltejs/kit';
+import { google } from 'googleapis';
+
+export interface MovieRecommendation {
+	content: Content;
+	similarity: number;
+}
+
+export interface Content {
+	DVD: string;
+	Plot: string;
+	Type: string;
+	Year: string;
+	Genre: string;
+	Rated: string;
+	Title: string;
+	Actors: string;
+	Awards: string;
+	Poster: string;
+	Writer: string;
+	imdbID: string;
+	Country: string;
+	Ratings: string[];
+	Runtime: string;
+	Website: string;
+	Director: string;
+	Language: string;
+	Released: string;
+	Response: string;
+	BoxOffice: string;
+	Metascore: string;
+	imdbVotes: string;
+	Production: string;
+	imdbRating: string;
+}
+
+const youtubeApi = google.youtube({
+	version: 'v3',
+	auth: YOUTUBE_API_KEY
+});
 
 export const POST = (async ({ request }) => {
 	// Extract the `prompt` from the body of the request
@@ -25,14 +65,47 @@ export const POST = (async ({ request }) => {
 		throw error(400, matchError.message);
 	}
 
-	console.log(movieMatches);
+	const typedMovieMatches = movieMatches.map((movieMatch) => {
+		return movieMatch as unknown as MovieRecommendation;
+	});
 
-	const prompt = `You are a very enthusiastic movie specialist who loves to recommend people the right movies based on their mood. Given the following json objects (which represent the movies the user owns), answer the users question below or make a recommendation if you think it's appropriate.
+	const fullData = await Promise.all(
+		typedMovieMatches.map(async ({ content }) => {
+			const { data } = await youtubeApi.search.list({
+				part: ['snippet'],
+				q: `${content.Title} ${content.Year} trailer`,
+				type: ['video'],
+				maxResults: 1
+			});
+			console.log(
+				content.Title,
+				`https://www.youtube.com/watch?v=${
+					data.items && data.items[0] && data.items[0].id ? data.items[0].id.videoId : ''
+				}`
+			);
+			return {
+				...content,
+				'youtube-link':
+					data.items && data.items[0] && data.items[0].id
+						? `https://www.youtube.com/watch?v=${data.items[0].id.videoId}`
+						: 'N/A'
+			};
+		})
+	);
+
+	// const videoId = data.items && data.items[0] && data.items[0].id ? data.items[0].id.videoId : "";
+
+	const prompt = `You are a very enthusiastic movie specialist who loves to recommend people the right movies based on their mood. 
+Given the following json objects (which represent the movies the user owns), answer the users question below or make a recommendation if you think it's appropriate.
+If you are asked to format a link to a YouTube trailer, always use HTML tags (<a href='[link]' >[text]</a>) and never markdown.
 Never refer to the json objects directly, but rather use the information in them to answer the question. 
+Provide the trailers to YouTube in a nicely formatted call to action, wrapped in a HTML (never use markdown to format the link) anchor tags and put a class of "youtube-trailer" on the anchor tags.
 Also give a reason for your answer.
-Finally, give a short list of alternative movies that the user owns. Never recommend a movie that the user doesn't own. If there are no alternatives, don't give alternatives.
+Finally, give a short list of alternative movies that the user owns. Never recommend a movie that the user doesn't own. 
+If there are no alternatives, don't give alternatives.
+Never recommend a movie that the user doesn't own which are listed below.
 Context:
-${JSON.stringify(movieMatches.map(({ content }) => content))}
+${JSON.stringify(fullData)}
 
 Question: """
 ${messages[messages.length - 1].content}
